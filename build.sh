3bc055f11666e56177d8d129c7342ff9a591e764
Justin Donato <justin.donato@gmail.com>
2015-01-03 17:36:15 -0500

    add font and back button with font

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 T="$(date +%s)"
 
 zip old-deploys/replaced-by-$T.zip deploy/*
+rm -rf deploy/font/
 rm deploy/*
 
 browserify main.js -o deploy/main.$T.js
-cp main.css deploy/main.css
+cp *.css deploy/
+cp -r font/ deploy/font/
 sed s/TIMESTAMP/$T/g index.html > deploy/index.html
 sed s/TIMESTAMP/$T/g /home/justin/projects/miniperdiem/cache.manifest.template > /home/justin/projects/miniperdiem/deploy/cache.manifest
 
